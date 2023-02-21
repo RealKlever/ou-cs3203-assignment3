5339944c8ef8d6757f9ffd0dce72837d5f948d2c
kenneththompson0220@gmail.com <kenneththompson0220@gmail.com>
2023-02-20 20:58:14 -0600

    Added main method and called methods

--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -8,6 +8,25 @@ namespace HW3
 {
     class HW3
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int arrayLength = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[arrayLength];
+
+            for(int i = 0; i < arrayLength; i++) 
+            { 
+                Console.Write("Enter number for arr[" + i + "]: ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sumArr(arr));
+            Console.WriteLine(multArr(arr));
+
+            Console.ReadKey();
+        }
+
         static int sumArr(int[] arr)
         {
             int sum = 0;
