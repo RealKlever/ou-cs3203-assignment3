fee5148a479602e55a491eb3650c22d5e4b606ab
kenneththompson0220@gmail.com <kenneththompson0220@gmail.com>
2023-02-20 20:49:58 -0600

    Added sumArr method

---
+++ HW3/HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class HW3
+    {
+        static int sumArr(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
